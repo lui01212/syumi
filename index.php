d9d07278038f6c24294811b41a86dfa70d951f47
lui01212 <bvl0165@gmail.com>
2018-07-02 21:14:43 +0700

    update index.php

--- index.php
+++ index.php
@@ -14,25 +14,4 @@
 define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
-//require( dirname( __FILE__ ) . '/wp-blog-header.php' );
-?>
-<?php
-$val = 'aloa';
-$stringInt = 'ssss22dsdsd'; 
-$ep = (int)$stringInt;
-$int = 2 + 3 ;
-define('luyn','oke');
-$boolean = true;
-echo  var_dump(is_int(@$stringInt));
-$array =  array('1' => 'kk','3' =>'aa');
-//print_r($array);
-echo "luyn ";
-var_dump($int >1);
-//for($ix = 0 ;$ix < count($array) ;$ix ++){
-//    echo $array[$ix];
-//}
-//foreach($array as $key => $indexx){
-//    echo  $key .'=>'.$indexx;
-//} 
-//oke
-?>
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
