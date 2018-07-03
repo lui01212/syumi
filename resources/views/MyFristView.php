54b2f6ae2de2035eb53a8ae89ae81e6a6905476f
lui01212 <bvl0165@gmail.com>
2018-07-03 20:58:13 +0700

    ADD VIEW AND CONTROLLER

---
+++ resources/views/MyFristView.php
@@ -0,0 +1,6 @@
+<h1>This is my first view</h1>
+<?php 
+	foreach ($kaka as $key => $value) {
+		echo $value;
+	}
+?>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/view/{name}/{age}','MyFristController@getController');
