931efb9437e0bd44f83e2e20752826d6cb1f48a4
lui01212 <bvl0165@gmail.com>
2018-07-03 17:08:22 +0700

    20180703

--- local/routes/web.php
+++ local/routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+ 
+Route::get('hello/{name?}/{age?}',function($name = 'null',$age = '1'){
+	echo "hello word!".$name.$age;
+});
+
+Route::group(['prefix' => 'admin'],function(){
+	Route::group(['prefix' => 'sebar'],function(){
+		Route::get('left',function(){
+				echo "this is left";
+		});
+	});
+	Route::get('home',function(){
+		echo "this is home";
+	});
+});
+Route::get('controller/{a}/{b}','MyfirstController@getController');
+Route::get('view','MyfirstController@getView');
